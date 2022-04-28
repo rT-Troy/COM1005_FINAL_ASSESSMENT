b4960ea46acdf45b273110199517f89149f36935
rT_Troy <yotroy@foxmail.com>
2022-04-28 04:00:33 +0100

    EpuzzleSearch.java

--- BFS/EpuzzleSearch.java
+++ BFS/EpuzzleSearch.java
@@ -1,2 +1,32 @@
-public class EpuzzleSearch {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EpuzzleSearch extends Search {
+
+    private int puzzleSer; //the serial number
+    private HashMap<Integer,Integer> puzzleList = new HashMap<Integer,Integer>();
+
+    //constructor-transfer int array to hashmap
+    public EpuzzleSearch(int[][] pg) {
+        int initKey = 0;
+        for (int i = 0; i < pg.length; i++) {
+            for (int j = 0; j < pg[i].length; j++) {
+                puzzleList.put(initKey,pg[i][j]);
+                initKey ++;
+            }
+        }
+
+    }
+    public EpuzzleSearch(HashMap<Integer,Integer> list) {
+        puzzleList = list;
+    }
+
+    public HashMap<Integer,Integer> getHashList(){
+        return puzzleList;
+    }
+
+
+
+
+
 }
--- BFS/EpuzzleState.java
+++ BFS/EpuzzleState.java
@@ -1,2 +1,22 @@
-public class EpuzzleState {
+import java.util.ArrayList;
+
+public class EpuzzleState extends SearchState{
+
+
+
+
+    @Override
+    boolean goalPredicate(Search searcher) {
+        return false;
+    }
+
+    @Override
+    ArrayList<SearchState> getSuccessors(Search searcher) {
+        return null;
+    }
+
+    @Override
+    boolean sameState(SearchState n2) {
+        return false;
+    }
 }
